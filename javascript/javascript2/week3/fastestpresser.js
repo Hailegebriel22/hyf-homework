ea2fe29bbfd914bbeb138f90c1bcaece96509982
Hailegebriel22 <eartalea@gmail.com>
2022-07-09 13:10:02 +0200

    shorter variable declaration

--- javascript/javascript2/week3/fastestpresser.js
+++ javascript/javascript2/week3/fastestpresser.js
@@ -1,15 +1,17 @@
-let timerValue = document.getElementById("set-timer").value;
+let timer = document.getElementById("set-timer");
 let setTimer = document.querySelector("#set-timer");
+let resultS=document.getElementById("result-s");
+let resultL=document.getElementById("result-l")
 
 let startBtn = document.querySelector("#start-btn")
 
 function fastestPresser() {
 
-    if (document.getElementById("set-timer").value === "") {
+    if (timer.value === "") {
 
         notetext.innerHTML = "please enter valid time to play!";
     }
-    else if (!isNaN(document.getElementById("set-timer").value)) {
+    else if (!isNaN(timer.value)) {
         notetext.innerHTML = "The game starts now!";
     }
 
@@ -21,11 +23,11 @@ let counters = 1;
 function keyPressCounter() {
     document.addEventListener("keypress", e => {
 
-        if (e.key === "l" && document.getElementById("set-timer").value !== "") {
-            document.getElementById("result-l").innerHTML = `${counterl++}`;
+        if (e.key === "l" && timer.value !== "") {
+            resultL.innerHTML = `${counterl++}`;
         }
-        else if (e.key === "s" && document.getElementById("set-timer").value !== "") {
-            document.getElementById("result-s").innerHTML = `${counters++}`;
+        else if (e.key === "s" && timer.value !== "") {
+            resultS.innerHTML = `${counters++}`;
         }
 
     })
@@ -36,28 +38,28 @@ const result = () => {
 
         if (counterl > counters) {
 
-            document.getElementById("result-l").innerHTML = "congratulations You Won!";
+            resultL.innerHTML = "congratulations You Won!";
         }
         else if (counterl < counters) {
 
 
-            document.getElementById("result-s").innerHTML = "congratulations You Won!";
+            resultS.innerHTML = "congratulations You Won!";
 
         }
-        else if (document.getElementById("set-timer").value !== "" && counterl === counters) {
-            document.getElementById("result-s").innerHTML = " It's a Draw";
-            document.getElementById("result-l").innerHTML = " It's a Draw";
+        else if (timer.value !== "" && counterl === counters) {
+            resultL.innerHTML = " It's a Draw";
+            resultS.innerHTML = " It's a Draw";
 
 
         }
-    }, document.getElementById("set-timer").value * 1000)
+    },timer.value * 1000)
 
 }
 
 const reset = () => {
     setTimeout(() => {
         window.location.reload();
-    }, (document.getElementById("set-timer").value * 1000) + 5000)
+    }, (timer.value * 1000) + 5000)
 
 }
 
