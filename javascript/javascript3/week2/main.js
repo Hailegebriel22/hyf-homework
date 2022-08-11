3cf3724e063840b113c285c6b1099a3d8d377168
Hailegebriel22 <eartalea@gmail.com>
2022-08-11 12:11:25 +0200

    fix overwritten  file week2Js3 to main.js

--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -1,103 +1,46 @@
-/* Fetch movies from this api:
+const redCircle = document.querySelector('ul.marks li:nth-child(1)');
+const blueCircle = document.querySelector('ul.marks li:nth-child(2)');
+const greenCircle = document.querySelector('ul.marks li:nth-child(3)');
+const span = document.querySelector('span');
 
-Create an array of bad movies
-Creat an array of bad movies since year 2000 */
-function badMoviesRating() {
-    fetch("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json")
-        .then(Response => Response.json())
-        .then(movies => {
-            const badMovies = movies.filter(movie => movie.rating <= 4)
-            console.log(badMovies)
-            badMoviesSince2000 = badMovies.filter(badMovies => badMovies.year >= 2000)
-            console.log(badMoviesSince2000)
-
-
-        })
-}
-badMoviesRating()
-//const moviejson
-//console.log(movies.json())
-
-function resolveTimer(resolveAfter) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, resolveAfter * 1000);
-    });
-}
-resolveTimer(5).then(() => {
-    console.log("I am called asynchronously")
-})
-
-function setTimeoutPromises(miliseconds) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, miliseconds);
-    })
-}
-setTimeoutPromises(3000).then(() => {
-    console.log("Called after 3 seconds");
-});
-
-
-function getCurrentLocation() {
-    return new Promise(function (resolve, reject) {
-
-
-        // Promisifying the geolocation API
-        navigator.geolocation.getCurrentPosition(
-            (pos) => resolve(`lat-${pos.coords.latitude} long-${pos.coords.longitude}`),
-            (error) => reject(`error getting the user location`)
-        );
-    });
+const Circles = [redCircle, blueCircle, greenCircle];
+function circleMove(xBox, circleFrame) {
+    return {
+        x: circleFrame.x - (xBox.getBoundingClientRect()).left,
+        y: circleFrame.y - (xBox.getBoundingClientRect()).top
+    }
 };
 
-getCurrentLocation()
-    .then((pos) => {
-        // called when the users position is found
-        console.log(pos);
+
+
+// target position to move 
+const redTarget = circleMove(redCircle, { x: 20, y: 300 });
+const blueTarget = circleMove(blueCircle, { x: 400, y: 300 });
+const greenTarget = circleMove(greenCircle, { x: 400, y: 20 });
+async function translateOneByOne() {
+    await moveElement(redCircle, redTarget).then(() => {
+        console.log("Red circle has been moved");
     })
-    .catch((error) => {
-        // called if there was an error getting the users location
-        console.log(error);
-    });
-
-function getData() {
-    fetch('http://api.open-notify.org/astros.json')
-        .then(res => res.json())
-        .then(astronauts => {
-            console.log(astronauts);
-        }
-        )
-}
-
-function thenFetching() {
-    //fetch('http://api.open-notify.org/astros.json')
-    return new Promise((resolve, reject) => {
-        const url = 'http://api.open-notify.org/astros.json';
-        if (url) {
-            setTimeout(() => {
-                resolve()
-                fetch(url)
-                    .then(res => console.log(res.json()))
-
-            }, 3000);
-        }
-        else {
-            reject("error")
-        }
-    });
-}
-
-thenFetching()
-
-async function awaitFetching() {
-
-    const res = await fetch('http://api.open-notify.org/astros.json')
-    const astronauts = await res.json();
-    console.log(astronauts);
+    await moveElement(blueCircle, blueTarget).then(() => {
+        console.log("Blue circle has been moved");
+    })
+    await moveElement(greenCircle, greenTarget).then(() => {
+        console.log("Green circle has been moved");
+    })
 
 }
-setTimeout(awaitFetching, 3000);
+//translateOneByOne()
 
+function translateAllAtOnce() {
+    const promise1 = moveElement(redCircle, redTarget)
+    const promise2 = moveElement(blueCircle, blueTarget)
+    const promise3 = moveElement(greenCircle, greenTarget)
+    Promise.all([promise1, promise2, promise3])
+        .then(() => {
+            console.log("All circles have moved their targets at once.");
+        })
+
+}
+
+
+translateAllAtOnce()
