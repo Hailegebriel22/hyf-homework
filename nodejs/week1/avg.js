e9837afcd8df28737f03e5bb99e9b033ee5f1209
Hailegebriel22 <eartalea@gmail.com>
2022-09-12 08:16:40 +0200

    feedback corrections

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,13 +2,10 @@
 const array = process.argv.slice(2).map(num => Number(num));
 if (array.length === 0) {
     console.log('No arguments are provided')
-}
-
-else if (array.every(element => !isNaN(element))) {
+} else if (array.every(element => !isNaN(element))) {
     const average = array.reduce((a, b) => a + b) / array.length;
     console.log(average)
-}
-else {
+} else {
     console.log('Provided arguments are not numbers')
 }
 // / (process.argv.length-2)
